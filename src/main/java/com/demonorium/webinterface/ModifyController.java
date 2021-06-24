aa398486fda9a09e2770f0428d4c0a37b82fcab1
Demonorium <Demonorium@yandex.ru>
2021-06-24 17:38:32 +0300

    INIT

--- src/main/java/com/demonorium/webinterface/ModifyController.java
+++ src/main/java/com/demonorium/webinterface/ModifyController.java
@@ -11,8 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
-import java.lang.reflect.Parameter;
+
 
 @Controller
 public class ModifyController {
